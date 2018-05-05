025ff4a0be19cb4719387527d6915a9da9c327e4
mohadoop <34073312+mohadoop@users.noreply.github.com>
2018-05-05 05:50:20 -0400

    added jobs directory for accomodate jobs and moved import modules to import directory

---
+++ src/imports/hive/hive.py
@@ -0,0 +1,25 @@
+import findspark
+findspark.init()
+import pyspark
+from pyspark.sql import SQLContext
+from pyspark.sql import HiveContext
+
+# Initializing
+sc = pyspark.SparkContext(appName = "app_name")
+sqlContext = SQLContext(sc)
+hc = HiveContext(sc)
+
+spark = SparkSession
+        .builder()
+        .appName("interfacing spark sql to hive metastore without configuration file")
+        .config("hive.metastore.uris", "thrift://localhost:9083") // replace with your hivemetastore service's thrift url
+        .enableHiveSupport() // don't forget to enable hive support
+        .getOrCreate()
+
+fdmrt_cstmr = hc.table("foodmart.customer")
+fdmrt_cstmr.show()
+fdmrt_cstmr.registerTempTable("fdmrt_temp")
+hc.sql("select * from fdmrt_temp limit 5").show()
+fdmrt.write.format("orc").save("/imported/hive/data_from_hive_orc")
+fdmrt.write.format("csv").save("/imported/hive/data_from_hive_csv")
+print("the end")
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from jobs import *
 
 test.testing()
 
+# Importing
+print("Importing data from all the defined data sources")
 try:
 	hive.import()
 	hbase.import()
@@ -11,5 +13,11 @@ try:
 except Exception as ex:
         print(ex)
 	
+# Job execution
+print("Executing jobs")
+try:
+	join.master_join
+except Exception as ex:
+        print(ex)
 
 
